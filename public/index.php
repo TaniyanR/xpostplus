31dfd35afde3b0e8358ae6d47bb62e77ea03e1ef
TaniyanR <toolhouse@24.am>
2026-07-22 22:50:01 +0900

    画面構成に合わせてルートを整理

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 use App\Core\{App, Router};
-use App\Controllers\{AuthController, DashboardController, PostController, RssPostController, SiteController, TemplateController};
+use App\Controllers\{AuthController, DashboardController, PostController, SiteController, TemplateController};
 
 require dirname(__DIR__) . '/app/Core/bootstrap.php';
 
@@ -12,17 +12,23 @@ $router->get('/', [DashboardController::class, 'index']);
 $router->get('/login', [AuthController::class, 'showLogin']);
 $router->post('/login', [AuthController::class, 'login']);
 $router->post('/logout', [AuthController::class, 'logout']);
-$router->get('/password', [AuthController::class, 'showPassword']);
+
+$router->get('/settings', [AuthController::class, 'showSettings']);
+$router->post('/settings/email', [AuthController::class, 'changeEmail']);
+$router->post('/settings/password', [AuthController::class, 'changePassword']);
+$router->get('/password', [AuthController::class, 'showSettings']);
 $router->post('/password', [AuthController::class, 'changePassword']);
-$router->get('/rss-posts', [RssPostController::class, 'index']);
+
+$router->get('/rss-posts', [SiteController::class, 'index']);
+$router->post('/rss-posts', [SiteController::class, 'store']);
+$router->post('/rss-posts/delete', [SiteController::class, 'delete']);
+
 $router->get('/templates', [TemplateController::class, 'index']);
 $router->post('/templates', [TemplateController::class, 'store']);
 $router->post('/templates/delete', [TemplateController::class, 'delete']);
+
 $router->get('/posts', [PostController::class, 'index']);
-$router->post('/posts/generate', [PostController::class, 'generate']);
+$router->post('/posts/status', [PostController::class, 'updateStatus']);
 $router->post('/posts/delete', [PostController::class, 'delete']);
-$router->get('/sites', [SiteController::class, 'index']);
-$router->post('/sites', [SiteController::class, 'store']);
-$router->post('/sites/delete', [SiteController::class, 'delete']);
 
 (new App($router))->run();
